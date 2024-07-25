f85dd39312a501747b96fc35a07da4902906be11
Sherif-toiema22 <sherif.toiema22@gmail.com>
2024-07-25 23:53:40 +0300

    still trying :)

--- main.cpp
+++ main.cpp
@@ -1,27 +1,60 @@
 #include <bits/stdc++.h>
-#define LetsGo ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-using namespace std;
-//https://codeforces.com/contest/279/problem/B
-int main()
-{
- LetsGo;
- int x,y;cin>>x>>y;
- int sum=0,arr[y],l=0,r=0,mx=0;
-    for (int i = 0; i < x; ++i) {
-        cin>>arr[i];
-    }
-    sum+=arr[l];
- while(r<x) {
-     if(sum>y){
-         sum-=arr[l];l++;
-     }
-     else {
-         mx = r - l;
-         r++;
-         sum += arr[r];
 
-     }
- }
- cout<<mx;
- return 0;
+using namespace std;
+
+int const OO = 2e9;
+int const N = 1e6 + 1;
+int n, coins[N], dp[N];
+
+//int rec(int rem) {
+//    if (rem == 0) {
+//        return 0;
+//    }
+//
+//    if (rem < 0) {
+//        return OO;
+//    }
+//
+//    int &res = dp[rem];
+//    if (res != -1) {
+//        return res;
+//    }
+//
+//    res = OO;
+//
+//    int tmp = rem;
+//    while(tmp != 0) {
+//        res = min(res, rec(rem - (tmp % 10)) + 1);
+//        tmp /= 10;
+//    }
+//
+//    return res;
+//}
+int i=10;
+int DIF(int rem,int i){
+    if(rem == 0){
+        return 0;
+    }
+    if (rem < 0) {
+        return OO;
+    }
+
+    int &res = dp[rem];
+    if(res != -1){
+        return res;
+    }
+    res=OO;
+    int op1 = DIF(rem-rem%i , i*10);
+    int op2 = DIF(rem , i*10);
+    return min(op1, op2);
+}
+
+int main() {
+    scanf("%d", &n);
+
+    memset(dp, -1, sizeof dp);
+
+    printf("%d\n", DIF(n,10));
+
+    return 0;
 }
