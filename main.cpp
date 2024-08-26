6e2cb58603d6ed3146db1388fbbe5042a15f0af0
Sherif-toiema22 <sherif.toiema22@gmail.com>
2024-08-26 14:28:41 +0300

    Recursive way

--- main.cpp
+++ main.cpp
@@ -2,59 +2,37 @@
 
 using namespace std;
 
-int const OO = 2e9;
-int const N = 1e6 + 1;
-int n, coins[N], dp[N];
+const int X=1e6,N=1e2 ;
+int coins[N];
+int dp[X];
+int OO=2e9;
+int n,x;
+int minCoins(int rem){
+    if(rem == 0)return 0;
+    if( rem < 0) return OO;
 
-//int rec(int rem) {
-//    if (rem == 0) {
-//        return 0;
-//    }
-//
-//    if (rem < 0) {
-//        return OO;
-//    }
-//
-//    int &res = dp[rem];
-//    if (res != -1) {
-//        return res;
-//    }
-//
-//    res = OO;
-//
-//    int tmp = rem;
-//    while(tmp != 0) {
-//        res = min(res, rec(rem - (tmp % 10)) + 1);
-//        tmp /= 10;
-//    }
-//
-//    return res;
-//}
-int i=10;
-int DIF(int rem,int i){
-    if(rem == 0){
-        return 0;
-    }
-    if (rem < 0) {
-        return OO;
-    }
-
-    int &res = dp[rem];
+    int &res=dp[rem];
     if(res != -1){
         return res;
     }
     res=OO;
-    int op1 = DIF(rem-rem%i , i*10);
-    int op2 = DIF(rem , i*10);
-    return min(op1, op2);
+    for (int i = 0; i < n; ++i) {
+        res=min(res,minCoins(rem-coins[i])+1);
+
+    }
+    return res;
 }
+int main(){
+    cin>>n>>x;
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    }
+    memset(dp,-1,sizeof dp);
 
-int main() {
-    scanf("%d", &n);
-
-    memset(dp, -1, sizeof dp);
-
-    printf("%d\n", DIF(n,10));
-
+    int res= minCoins(x);
+    if(res==OO)
+        puts("-1");
+    else
+        cout<<res;
     return 0;
 }
